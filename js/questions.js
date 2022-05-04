7c9d87192b171e34b4ed1b02793295a1a55ef846
mcflaytrap <egeberk20@hotmail.com>
2022-05-04 20:33:02 +0300

    test

---
+++ js/questions.js
@@ -0,0 +1,62 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "When you go somewhere for the day, would you rather..",
+    answer: "WHEN YOU GO SOMEWHERE FOR THE DAY, WOULD YOU RATHER",
+    options: [
+      "Plan what you will and when",
+      "Just Go!"
+    ]
+  },
+    {
+    numb: 2,
+    question: "When you go somewhere for the day, would you rather..",
+    answer: "WHEN YOU GO SOMEWHERE FOR THE DAY, WOULD YOU RATHER",
+    options: [
+      "Plan what you will and when",
+      "Just Go!"
+    ]
+  },
+    {
+    numb: 3,
+    question: "When you go somewhere for the day, would you rather..",
+    answer: "WHEN YOU GO SOMEWHERE FOR THE DAY, WOULD YOU RATHER",
+    options: [
+      "Plan what you will and when",
+      "Just Go!"
+    ]
+  },
+    {
+    numb: 4,
+    question: "When you go somewhere for the day, would you rather..",
+    answer: "WHEN YOU GO SOMEWHERE FOR THE DAY, WOULD YOU RATHER",
+    options: [
+      "Plan what you will and when",
+      "Just Go!"
+    ]
+  },
+    {
+    numb: 5,
+    question: "When you go somewhere for the day, would you rather..",
+    answer: "WHEN YOU GO SOMEWHERE FOR THE DAY, WOULD YOU RATHER",
+    options: [
+      "Plan what you will and when",
+      "Just Go!"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
