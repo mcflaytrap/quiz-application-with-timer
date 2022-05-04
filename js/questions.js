36eac7d8c0eb52cda2038ec6d2ee4b168811bf56
mcflaytrap <53799748+mcflaytrap@users.noreply.github.com>
2022-05-04 22:07:59 +0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -2,49 +2,264 @@
 let questions = [
     {
     numb: 1,
-    question: "When you go somewhere for the day, would you rather..",
+    question: "WHEN YOU GO SOMEWHERE FOR THE DAY, WOULD YOU RATHER..",
     answer: "WHEN YOU GO SOMEWHERE FOR THE DAY, WOULD YOU RATHER",
     options: [
-      "Plan what you will and when",
-      "Just Go!"
+      "A. Plan what you will and when",
+      "B. Just Go!"
     ]
   },
     {
     numb: 2,
-    question: "When you go somewhere for the day, would you rather..",
-    answer: "WHEN YOU GO SOMEWHERE FOR THE DAY, WOULD YOU RATHER",
+    question: "IF YOU WERE A TEACHER, WOULD YOU RATHER TEACH..",
+    answer: "IF YOU WERE A TEACHER, WOULD YOU RATHER TEACH",
     options: [
-      "Plan what you will and when",
-      "Just Go!"
+      "A. FACTS-BASED COURSES, OR",
+      "B. COURSES INVOLVING OPINION OR THEORY?"
     ]
   },
     {
     numb: 3,
-    question: "When you go somewhere for the day, would you rather..",
-    answer: "WHEN YOU GO SOMEWHERE FOR THE DAY, WOULD YOU RATHER",
+    question: "ARE YOU USUALLY..",
+    answer: "ARE YOU USUALLY",
     options: [
-      "Plan what you will and when",
-      "Just Go!"
+      "A. A “GOOD MIXER” WITH GROUPS OF PEOPLE, OR",
+      "B. RATHER QUIET AND RESERVED?"
     ]
   },
     {
     numb: 4,
-    question: "When you go somewhere for the day, would you rather..",
-    answer: "WHEN YOU GO SOMEWHERE FOR THE DAY, WOULD YOU RATHER",
+    question: "DO YOU MORE OFTEN LET..",
+    answer: "DO YOU MORE OFTEN LET",
     options: [
-      "Plan what you will and when",
-      "Just Go!"
+      "A. YOUR HEART RULE YOUR HEAD. OR",
+      "B. YOUR HEAD RULE YOUR HEART?"
     ]
   },
     {
     numb: 5,
-    question: "When you go somewhere for the day, would you rather..",
-    answer: "WHEN YOU GO SOMEWHERE FOR THE DAY, WOULD YOU RATHER",
+    question: "IN DOING SOMETHING THAT MANY OTHER PEOPLE DO, WOULD YOU 
+RATHER..",
+    answer: "IN DOING SOMETHING THAT MANY OTHER PEOPLE DO, WOULD YOU 
+RATHER",
     options: [
-      "Plan what you will and when",
-      "Just Go!!"
+      "A. INVENT A WAY OF YOUR OWN, OR",
+      "B. DO IT IN THE ACCEPTED WAY?"
     ]
   },
+   {
+    numb: 6,
+    question: "AMONG YOUR FRIENDS ARE YOU..",
+    answer: "AMONG YOUR FRIENDS ARE YOU",
+    options: [
+      "A. FULL OF NEWS ABOUT EVERYBODY, OR",
+      "B. ONE OF THE LAST TO HEAR WHAT IS GOING ON?"
+    ]
+  },
+   {
+    numb: 7,
+    question: "DOES THE IDEA OF MAKING A LIST OF WHAT YOU SHOULD GET DONE 
+OVER A WEEKEND..",
+    answer: "DOES THE IDEA OF MAKING A LIST OF WHAT YOU SHOULD GET DONE 
+OVER A WEEKEND",
+    options: [
+      "A. HELP YOU, OR",
+      "B. STRESS YOU, OR",
+      "C. POSITIVELY DEPRESS YOU?"
+    ]
+  },
+ {
+    numb: 8,
+    question: "WHEN YOU HAVE A SPECIAL JOB TO DO, DO YOU LIKE TO..",
+    answer: "WHEN YOU HAVE A SPECIAL JOB TO DO, DO YOU LIKE TO",
+    options: [
+      "A. ORGANIZE IT CAREFULLY BEFORE YOU START, OR",
+      "B. FIND OUT WHAT IS NECESSARY AS YOU GO ALONG?"
+    ]
+  },
+{
+    numb: 9,
+    question: "DO YOU TEND TO HAVE..",
+    answer: "DO YOU TEND TO HAVE",
+    options: [
+      "A. BROAD FRIENDSHIPS WITH MANY DIFFERENT PEOPLE, OR",
+      "B. DEEP FRIENDSHIP WITH VERY FEW PEOPLE?"
+    ]
+  },
+{
+    numb: 10,
+    question: "DO YOU ADMIRE MORE THE PEOPLE WHO ARE..",
+    answer: "DO YOU TEND TO HAVE",
+    options: [
+      "A. NORMAL-ACTING TO NEVER MAKE THEMSELVES THE 
+CENTER OF ATTENTION, OR",
+      "B. TOO ORIGINAL AND INDIVIDUAL TO CARE WHETHER THEY 
+ARE THE CENTER OF ATTENTION OR NOT"
+    ]
+  },
+{
+    numb: 11,
+    question: "DO YOU PREFER TO..",
+    answer: "DO YOU PREFER TO",
+    options: [
+      "A. ARRANGE PICNICS, PARTIES ETC, WELL IN ADVANCE, OR",
+      "B. BE FREE TO DO WHATEVER TO LOOKS LIKE FUN WHEN 
+THE TIME COMES?"
+    ]
+  },
+{
+    numb: 12,
+    question: "DO YOU USUALLY GET ALONG BETTER WITH..",
+    answer: "DO YOU USUALLY GET ALONG BETTER WITH",
+    options: [
+      "A. REALISTIC PEOPLE, OR",
+      "B. IMAGINATIVE PEOPLE?"
+    ]
+  },
+{
+    numb: 13,
+    question: "WHEN YOU ARE WITH THE GROUP OF PEOPLE, WOULD YOU USUALLY 
+RATHER..",
+    answer: "WHEN YOU ARE WITH THE GROUP OF PEOPLE, WOULD YOU USUALLY 
+RATHER",
+    options: [
+      "A. JOIN IN THE TALK OF THE GROUP OR",
+      "B. STAND BACK AND LISTEN FIRST?"
+    ]
+  },
+{
+    numb: 14,
+    question: "IS IT A HIGHER COMPLIMENT TO BE CALLED..",
+    answer: "IS IT A HIGHER COMPLIMENT TO BE CALLED",
+    options: [
+      "A. A PERSON OF REAL FEELING, OR",
+      "B. A CONSISTENTLY REASONABLE PERSON?"
+    ]
+  },
+{
+    numb: 15,
+    question: "IN READING FOR PLEASURE, DO YOU..",
+    answer: "IN READING FOR PLEASURE, DO YOU",
+    options: [
+      "A. ENJOY ODD OR ORIGINAL WAYS OF SAYING THINGS, OR",
+      "B. LIKE WRITERS TO SAY EXACTLY WHAT THEY MEAN?"
+    ]
+  },
+{
+    numb: 16,
+    question: "DO YOU..",
+    answer: "DO YOU",
+    options: [
+      "A. TALK EASILY TO ALMOST ANYONE FOR AS LONG AS YOU 
+HAVE TO, OR",
+      "B. FIND A LOT TO SAY ONLY TO CERTAIN PEOPLE OR UNDER 
+CERTAIN CONDITIONS?"
+    ]
+  },
+{
+    numb: 17,
+    question: "DOES FOLLOWING A SCHEDULE..",
+    answer: "DOES FOLLOWING A SCHEDULE",
+    options: [
+      "A. APPEAL TO YOU, OR ",
+      "B. CRAMP YOU?"
+    ]
+  },
+{
+    numb: 18,
+    question: "WHEN IT IS SETTLED WELL IN ADVANCE THAT YOU WILL DO A 
+CERTAIN THING AT A CERTAIN TIME,DO YOU FIND IT..",
+    answer: "WHEN IT IS SETTLED WELL IN ADVANCE THAT YOU WILL DO A 
+CERTAIN THING AT A CERTAIN TIME,DO YOU FIND IT",
+    options: [
+      "A. NICE TO BE ABLE TO PLAN ACCORDINGLY, OR ",
+      "B. A LITTLE UNPLEASANT TO BE TIED DOWN?"
+    ]
+  },
+{
+    numb: 19,
+    question: "ARE YOU MORE SUCCESSFUL..",
+    answer: "ARE YOU MORE SUCCESSFUL",
+    options: [
+      "A. AT FOLLOWING A CAREFULLY WORKED OUT PLAN, OR ",
+      "B. AT DEALING WITH THE UNEXPECTED AND SEEING 
+QUICKLY WHAT SHOULD BE DONE?"
+    ]
+  },
+{
+    numb: 20,
+    question: "WOULD YOU RATHER BE CONSIDERED..",
+    answer: "WOULD YOU RATHER BE CONSIDERED",
+    options: [
+      "A. A PRACTICAL PERSON, OR",
+      "B. AN OUT-OF-THE-BOX-THINKING PERSON?"
+    ]
+  },
+{
+    numb: 21,
+    question: "IN A LARGE GROUP, DO YOU MORE OFTEN..",
+    answer: "IN A LARGE GROUP, DO YOU MORE OFTEN",
+    options: [
+      "A. INTRODUCE OTHERS, OR",
+      "B. GET INTRODUCED?"
+    ]
+  },
+{
+    numb: 22,
+    question: "DO YOU USUALLY..",
+    answer: "DO YOU USUALLY",
+    options: [
+      "A. VALUE EMOTION MORE THAN LOGIC, OR",
+      "B. VALUE LOGIC MORE THAN FEELINGS?"
+    ]
+  },
+{
+    numb: 23,
+    question: "WOULD YOU RATHER HAVE AS A FRIEND..",
+    answer: "WOULD YOU RATHER HAVE AS A FRIEND",
+    options: [
+      "A. SOMEONE WHO IS ALWAYS COMING UP WITH NEW 
+IDEAS, OR",
+      "B. SOMEONE WHO HAS BOTH FEET ON THE GROUND?"
+    ]
+  },
+{
+    numb: 24,
+    question: "CAN THE NEW PEOPLE YOU MEET TELL WHAT YOU ARE INTERESTED 
+IN..",
+    answer: "CAN THE NEW PEOPLE YOU MEET TELL WHAT YOU ARE INTERESTED 
+IN",
+    options: [
+      "A. RIGHT AWAY, ",
+      "B. ONLY AFTER THEY REALLY GET TO KNOW YOU?"
+    ]
+  },
+{
+    numb: 25,
+    question: "(ON THIS QUESTION ONLY, IF TWO ANSWERS ARE TRUE, CIRCLE 
+BOTH)
+IN YOUR DAILY WORK, DO YOU..",
+    answer: "(ON THIS QUESTION ONLY, IF TWO ANSWERS ARE TRUE, CIRCLE 
+BOTH)
+IN YOUR DAILY WORK, DO YOU",
+    options: [
+      "A. USUALLY PLAN YOUR WORK SO YOU WON’T NEED TO 
+WORK UNDER PRESSURE, OR ",
+      "B. RATHER ENJOY AN EMERGENCY THAT MAKES YOU WORK 
+AGAINST TIME, OR",
+      "c. HATE TO WORK UNDER PRESSURE? "
+    ]
+  },
+{
+    numb: 26,
+    question: "DO YOU USUALLY..",
+    answer: "CDO YOU USUALLY",
+    options: [
+      "A. SHOW YOUR FEELINGS FREELY, OR ",
+      "B. KEEP YOUR FEELINGS TO YOURSELF??"
+    ]
+  },
+
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
